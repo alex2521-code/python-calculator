a78cb6ab1c920e585c15f0fbfb8a83e90c206b6b
alex2521-code <thanakrit.tae@mahidol.ac.th>
2025-11-13 02:32:39 +0000

    Test commit 2

--- test_calculator.py
+++ test_calculator.py
@@ -41,18 +41,18 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(5, 0), 0)
         self.assertEqual(self.calc.multiply(0, 5), 0)
 
-    def test_divide(self):
-        # integer division
-        self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertEqual(self.calc.divide(9, 2), 4)  # 9 // 2 == 4
-        self.assertEqual(self.calc.divide(0, 3), 0)
-        with self.assertRaises(ZeroDivisionError):
-            self.calc.divide(5, 0)
+    # def test_divide(self):
+    #     # integer division
+    #     self.assertEqual(self.calc.divide(10, 2), 5)
+    #     self.assertEqual(self.calc.divide(9, 2), 4)  # 9 // 2 == 4
+    #     self.assertEqual(self.calc.divide(0, 3), 0)
+    #     with self.assertRaises(ZeroDivisionError):
+    #         self.calc.divide(5, 0)
 
-    def test_modulo(self):
-        self.assertEqual(self.calc.modulo(10, 3), 1)
-        self.assertEqual(self.calc.modulo(9, 3), 0)
-        self.assertEqual(self.calc.modulo(2, 5), 2)
+    # def test_modulo(self):
+    #     self.assertEqual(self.calc.modulo(10, 3), 1)
+    #     self.assertEqual(self.calc.modulo(9, 3), 0)
+    #     self.assertEqual(self.calc.modulo(2, 5), 2)
 
 if __name__ == "__main__":
     unittest.main()
